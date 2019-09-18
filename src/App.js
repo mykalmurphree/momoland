f48c3afc5506c6c910f3cf1b49e53f8ebda1c7ab
Michael Murphree <michaelmurphree@Michaels-iMac.local>
2019-09-18 08:37:55 -0700

    button test

--- src/App.js
+++ src/App.js
@@ -2,22 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import styled, { css } from 'styled-components';
-
-const Button = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid palevioletred;
-  color: palevioletred;
-  margin: 0 1em;
-  padding: 0.25em 1em;
-
-  ${props =>
-    props.primary &&
-    css`
-      background: palevioletred;
-      color: white;
-    `};
-`
+import Button from './Button.jsx'
 
 function App() {
   return (
